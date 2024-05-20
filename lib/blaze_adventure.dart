514f39bba916ae61a7b01156adcc58d5f7ed63ed
Netesh5 <netesh.paudel@gmail.com>
2024-05-20 15:06:27 +0545

    feat: changed bg to full screen

--- lib/blaze_adventure.dart
+++ lib/blaze_adventure.dart
@@ -3,13 +3,27 @@ import 'dart:async';
 import 'package:blaze_adventure/levels/level.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
 class BlazeAdventure extends FlameGame {
   @override
   final world = Level();
+
+  @override
+  Color backgroundColor() => const Color(0xff211F30);
+
   @override
   FutureOr<void> onLoad() {
-    add(CameraComponent(world: world));
+    addAll(
+      [
+        CameraComponent.withFixedResolution(
+          width: 640,
+          height: 360,
+          world: world,
+        )..viewfinder.anchor = Anchor.topLeft,
+        world,
+      ],
+    );
     return super.onLoad();
   }
 }
